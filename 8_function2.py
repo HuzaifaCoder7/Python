976085d49a52fba8896fcb27307ec56c8a6e2c70
Huzaifa Amjad <amjadhuzaifa067@gmail.com>
2023-09-12 14:35:17 +0500

    Fixed code

--- 8_function2.py
+++ 8_function2.py
@@ -2,13 +2,11 @@ def average(*num):
     sum = 0
     for i in num:
         sum = sum + i
-        print("Average is: ",sum/len(num))
         
+    print("Average is: ",sum/len(num))
 
 
-choice = int(input("Enter numbers to give average: "))
-for j in range(choice):
-      c =  int(input("Enter:"))
+
       
+average(2,3,4)
 
-average(c)
