d2eeb21b7533e11e01b8f79cdb34da0833c44f8f
Huzaifa Amjad <amjadhuzaifa067@gmail.com>
2023-09-15 09:16:42 +0500

    ...

---
+++ 11_List_Methods.py
@@ -0,0 +1,16 @@
+
+numbers = [1,42,24,62,111,4,76,98]
+# numbers.sort() ==> Will sort out all list
+
+# numbers.sort(reverse=True) ==> Will sort out in decending order
+
+# numbers.append(33) ==> Will add new item in end
+
+# print(numbers.index(24)) ==> Will tell at what index given number is placed
+
+# print(numbers.count(24)) ==> Will count the given number in List
+
+number2 = [22,33,44,55]
+# allnum = numbers + number2 ==> Method of adding two lists
+# number2.insert(2,54) ==> Will insert 54 at 2 index
+print(number2)
---
+++ 12_Tuple.py
@@ -0,0 +1,8 @@
+tup = (1, 2 ,3, 4, 5)
+# print(tup)
+#! Note: Tuple cannot be changed unlike Lists. It starts with Parenthesis. You cannot add or remove value in Tuple. Although some methods like indexing, slicing, iterating, finding element are same as Lists.
+
+if 2 in tup:
+    print("Present")
+else:
+    print("Not Present")
