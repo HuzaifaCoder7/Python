ce77a39e50d6549a32ee23a079305770239aac13
Huzaifa <amjadhuzaifa067@gmail.com>
2023-10-11 18:54:54 +0500

    Done

---
+++ 13_dictionary.py
@@ -0,0 +1,18 @@
+students = {
+    1 : "Huzaifa",
+    2 : "Ali",
+    3 : "Usman",
+    4 : "Kaif",
+    5 : "Ahmed"
+}
+# print(students) --> To print all dictionary
+# print(students[1]) --> To print at specific key
+
+#! Iterating through Loop
+# print("Roll no   Students")
+# for keys,values in students.items():
+#     print(f"{keys}   {values}")
+
+# students.pop(3) --> Remove key 3 value
+# students.clear() --> Will clear all dictionary
+print(students)
