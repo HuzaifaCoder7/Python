0f1667273619feb42cc5b0ed998a0aa9147f68ea
Huzaifa <amjadhuzaifa067@gmail.com>
2023-10-11 19:19:25 +0500

    Done

---
+++ 14_for+with+else.py
@@ -0,0 +1,13 @@
+# for i in range(5):
+#     print(i)
+# else:
+#     print("Else block executed")
+# print("END of Loop") 
+
+i = 14
+while i < 10:
+    print(i)
+    i = i + 1
+else:
+    print("Number is out of range")
+print("End of LOop")
