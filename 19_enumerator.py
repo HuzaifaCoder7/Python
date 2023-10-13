b9acc475eb1e54ee3a2aa324618a74de292fd310
Huzaifa <amjadhuzaifa067@gmail.com>
2023-10-13 19:25:34 +0500

    done

---
+++ 19_enumerator.py
@@ -0,0 +1,7 @@
+marks = [12,44,54,99,43,55,43]
+for index,i in enumerate(marks):
+    print(i)
+    if(index == 3):
+        print("Huzaifa you are awesome")
+
+#! Sometimes in a loop at particular index we want to do something but without enumerator it is alittle bit difficult but with enumerate it is easy. You can also specify from where you start index.It also works with tuple and dict.
