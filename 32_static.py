766c999e2cac12a0a5b730898acfe2daf87369b1
Huzaifa <amjadhuzaifa067@gmail.com>
2023-10-17 19:23:44 +0500

    done

---
+++ 32_static.py
@@ -0,0 +1,8 @@
+class Math:
+    @staticmethod
+    def add(a,b):
+        print(a+b)
+    
+obj = Math.add(5,4)
+
+#! If you are creating static method then there is no need to write self in it but on other function it is mandatory.
